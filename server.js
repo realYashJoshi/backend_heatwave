33364e7743bcfe96ea3a64846bf6904907ddec28
YashJoshi <20me02020@gmail.com>
2025-05-11 21:43:34 +0530

    added cache

--- server.js
+++ server.js
@@ -4,6 +4,26 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
+const cache = {};
+// app.get('/api/forecast', async (req, res) => {
+//   const { lat, lon, days } = req.query;
+
+//   if (!lat || !lon || !days) {
+//     return res.status(400).json({ error: "Missing lat, lon or days" });
+//   }
+
+//   const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max&forecast_days=${days}&timezone=auto`;
+
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     res.json(data);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ error: "API request failed" });
+//   }
+// });
+
 app.get('/api/forecast', async (req, res) => {
   const { lat, lon, days } = req.query;
 
@@ -11,17 +31,36 @@ app.get('/api/forecast', async (req, res) => {
     return res.status(400).json({ error: "Missing lat, lon or days" });
   }
 
+  const today = new Date().toISOString().split('T')[0];
+  const cacheKey = `${lat}_${lon}_${days}_${today}`;
+
+  if (cache[cacheKey]) {
+    return res.json({
+      source: "cache",
+      message: "Data served from cache",
+      data: cache[cacheKey]
+    });
+  }
+
   const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max&forecast_days=${days}&timezone=auto`;
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    res.json(data);
+
+    cache[cacheKey] = data;
+
+    res.json({
+      source: "api",
+      message: "Data fetched from Open-Meteo API",
+      data: data
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "API request failed" });
   }
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
